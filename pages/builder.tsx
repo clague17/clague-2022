401873f95b8402213d1db0ccf1adf2f5c54a8901
clague <lclague9@gmail.com>
2022-02-01 10:29:02 -0700

    continuing to add my builder page

--- pages/builder.tsx
+++ pages/builder.tsx
@@ -30,54 +30,95 @@ export default function Builder() {
           </div>
         </div>
       </main>
-      <div className="flex w-[90%] flex-col border-black pl-2 text-center">
-        <div className="py-5">
+      <div className="flex w-full flex-col border-black">
+        <div className="py-5 text-center">
           <p className="text-4xl">excellent choice—welcome to my garage!</p>
           <p>
             TLDR; I'm a software engineer interested in accessibility, crypto,
             and cool shiny things that solve tough difficult problems
           </p>
         </div>
-        <div id="md:w-[80%] md:m-auto lg:w-[60%] justify-start">
-          <p className="text-7xl">January 2021</p>
-          <p className="text-left text-7xl">--</p>
-        </div>
-        <div className="flex flex-col py-3 md:m-auto md:w-[80%] md:flex-row md:flex-row lg:w-[60%]">
-          <div id="project" className="project-card">
-            <div className="flex justify-center border-b-2 border-black md:border-transparent">
-              <img
-                src="/poke-dex.gif"
-                alt="ricehouse"
-                className="flex h-64 object-cover md:max-w-[30vw]"
-              />
-            </div>
-            <div className="flex flex-col p-4 text-left">
-              <p className="text-lg font-light">
-                how do I get involved with blockchain?
-              </p>
-              <p className="text-3xl font-bold">pokéDEX</p>
-              <div className="flex w-full py-2">
-                <p className="border-2 border-black bg-info px-2">Typescript</p>
-                <p className="border-t-2 border-b-2 border-r-2 border-black bg-yellow-300 px-2">
-                  TailwindCSS
+        <div id="projects-list" className="w-30% bg-gray-400">
+          <div id="md:w-[80%] md:m-auto lg:w-[60%] text-left">
+            <p className="text-xl">January 2022</p>
+            <p className="text-xl">--</p>
+          </div>
+          <div className="flex flex-col py-3 md:m-auto md:w-[80%] lg:w-[60%]">
+            <div id="project" className="project-card">
+              <div className="card-thumbnail">
+                <img
+                  src="/new-poke-dex-cap.gif"
+                  alt="ricehouse"
+                  className="flex h-64 object-cover md:max-w-[40vw]"
+                />
+              </div>
+              <div className="flex flex-col p-4 text-left">
+                <p className="text-lg font-light">
+                  how do I get involved with blockchain?
                 </p>
-                <p className="border-t-2 border-b-2 border-r-2 border-black bg-orange px-2">
-                  Rust
+                <p className="text-3xl font-bold">pokéDEX</p>
+                <div className="flex w-full py-2">
+                  <p className="border-2 border-black bg-info px-2">
+                    Typescript
+                  </p>
+                  <p className="border-t-2 border-b-2 border-r-2 border-black bg-yellow-300 px-2">
+                    TailwindCSS
+                  </p>
+                  <p className="border-t-2 border-b-2 border-r-2 border-black bg-orange px-2">
+                    Rust
+                  </p>
+                </div>
+                <p>
+                  A fully functional decentralized exchange live on Solana
+                  devnet. Playground for learning about token accounts, PDAs,
+                  CPIs, and all things Solana development. Learned Rust basics
+                  to implement smart contract that regulates the escrow program
+                  necessary for the DEX.
                 </p>
               </div>
-              <p>
-                A fully functional decentralized exchange live on Solana devnet.
-                Playground for learning about token accounts, PDAs, CPIs, and
-                all things Solana development. Learned Rust basics to implement
-                smart contract that regulates the escrow program necessary for
-                the DEX.
-              </p>
+            </div>
+            <div className="project-card">
+              <div className="card-thumbnail">
+                <img
+                  src="/pokecandy-cap.gif"
+                  alt="ricehouse"
+                  className="flex h-64 object-cover md:max-w-[40vw]"
+                />
+              </div>
+              <div className="flex flex-col p-4 text-left">
+                <p className="text-lg font-light">
+                  how do I get involved with blockchain?
+                </p>
+                <p className="text-3xl font-bold">pokéDEX</p>
+                <div className="flex w-full py-2">
+                  <p className="border-2 border-black bg-info px-2">
+                    Typescript
+                  </p>
+                  <p className="border-t-2 border-b-2 border-r-2 border-black bg-yellow-300 px-2">
+                    TailwindCSS
+                  </p>
+                  <p className="border-t-2 border-b-2 border-r-2 border-black bg-orange px-2">
+                    Rust
+                  </p>
+                </div>
+                <p>
+                  A fully functional decentralized exchange live on Solana
+                  devnet. Playground for learning about token accounts, PDAs,
+                  CPIs, and all things Solana development. Learned Rust basics
+                  to implement smart contract that regulates the escrow program
+                  necessary for the DEX.
+                </p>
+              </div>
             </div>
           </div>
         </div>
+        <div id="md:w-[80%] md:m-auto lg:w-[60%] justify-start">
+          <p className="text-7xl">January 2021</p>
+          <p className="text-7xl">--</p>
+        </div>
         <div className="flex flex-col md:m-auto md:w-[80%] md:flex-row md:flex-row lg:w-[60%]">
           <div id="project" className="project-card">
-            <div className="flex justify-center border-b-2 border-black md:border-transparent">
+            <div className="card-thumbnail">
               <img
                 src="/ricehouse.png"
                 alt="ricehouse"
@@ -97,7 +138,7 @@ export default function Builder() {
               </div>
               <p>
                 An ambitious project to escape to Hawaii and create content
-                during the pandemic of 2021. I built out the website to garner
+                during the pandemic of 2020. I built out the website to garner
                 interest and run the outreach to Rice students.
               </p>
             </div>
